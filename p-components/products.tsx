e186a2cf6899312a41bace5074f5cf039e40683b
Guido Pierri <105157860+Guido-Pierri@users.noreply.github.com>
2023-04-05 16:56:59 +0200

    database updated

--- p-components/products.tsx
+++ p-components/products.tsx
@@ -48,70 +48,10 @@ export default function Products() {
       })
   }, [])
 
-  // if (isLoading) return <div> Loading...</div>
-  // if (!data.length) return <p>No profile data</p>
+  if (isLoading) return <div> Loading...</div>
+  if (!data.length) return <p>No product data</p>
 
   {
-    /*
-const products = [
-  {
-    id: 1,
-    name: "Bike",
-    href: "#",
-    price: "$48",
-    imageSrc:
-      "https://i5.walmartimages.com/asr/3237dca1-47e1-42d3-9ff2-a0f7dccae087_1.8954232623d2ec2baae206fc64132e25.jpeg",
-    imageAlt:
-      "Mongoose 26 Bedlam 21 Speed Shimano Full Suspension Mens Mountain Bike.",
-  },
-  {
-    id: 2,
-    name: "Screw Driver",
-    href: "#",
-    price: "$35",
-    imageSrc:
-      "https://images-na.ssl-images-amazon.com/images/I/916YJyJGfdL.jpg",
-    imageAlt: "",
-  },
-  {
-    id: 3,
-    name: "Office Chair",
-    href: "#",
-    price: "$89",
-    imageSrc:
-      "https://i5.walmartimages.com/asr/88c4906f-0c30-40c7-aef6-b67ec3fbbdf0.85f44ef61cc777e88292ab6faaa7c402.jpeg",
-    imageAlt: "",
-  },
-  {
-    id: 4,
-    name: "Machined Mechanical Pencil",
-    href: "#",
-    price: "$35",
-    imageSrc:
-      "https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg",
-    imageAlt:
-      "Hand holding black machined steel mechanical pencil with brass tip and top.",
-  },
-  {
-    id: 5,
-    name: "Thermos",
-    href: "#",
-    price: "$15",
-    imageSrc:
-      "https://cdnimg.webstaurantstore.com/images/products/xxl/540645/1978166.jpg",
-    imageAlt: "",
-  },
-  {
-    id: 6,
-    name: "Waffle Maker",
-    href: "#",
-    price: "$15",
-    imageSrc:
-      "https://i5.walmartimages.com/asr/200b2856-a500-494d-9903-4d3f894fca0b.792b3b384eb97a8c51d55df8b4ac5e3c.jpeg?odnWidth=1000&odnHeight=1000&odnBg=ffffff",
-    imageAlt: "Waffle maker",
-  },
-  // More products...
-]*/
   }
 
   return (
