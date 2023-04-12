2e39831ce38d92d347cbe610f0f218c95cc3751c
Alex <Alex.Paulsson@stud.sti.se>
2023-04-12 12:47:28 +0200

    På börjat en "wave"

--- pages/index.tsx
+++ pages/index.tsx
@@ -26,16 +26,29 @@ interface Props {}
 
 const Index: NextPage<Props> = ({}) => {
   return (
-    <div className="">
-      <div className="">
-        <h1 className=" text-amber-600 w-72 h-20 text-7xl place-self-center font-minFont">
-          Borrow
-        </h1>
-        <p>Låna och låna ut inom din bostadsförening!</p>
-        <Link href={"/login"}>
-          <button>Kom igång!</button>
-        </Link>
+    <div className=" bg-[#F5F5F5] max-w-sm h-screen">
+      <div className="Wave1">
+        <div className="custom-shape-divider-top-1681296353">
+          <svg
+            data-name="Layer 1"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 1200 120"
+            preserveAspectRatio="none"
+          >
+            <path
+              d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
+              className="shape-fill"
+            ></path>
+          </svg>
+        </div>
       </div>
+      <h1 className=" text-amber-600 text-7xl place-self-center font-minFont">
+        Borrow
+      </h1>
+      <p>Låna och låna ut inom din bostadsförening!</p>
+      <Link href={"/login"}>
+        <button>Kom igång!</button>
+      </Link>
     </div>
   );
 };
