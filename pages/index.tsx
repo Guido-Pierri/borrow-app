ad08927509a01f7de791a0f52a77776e328b5b38
Alex <Alex.Paulsson@stud.sti.se>
2023-04-12 12:26:35 +0200

    La till knapp

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,41 @@
-import Products from "@/p-components/products";
-import Header from "@/p-components/header";
+// import Products from "@/p-components/products";
+// import Header from "@/p-components/header";
+// import { NextPage } from "next";
+// import Link from "next/link";
+// interface Props {}
+
+// const Index: NextPage<Props> = ({}) => {
+//   return (
+//     <div className="bg-[#F5F5F5] max-w-sm h-screen">
+//       <Header></Header>
+
+//       <section className="flex flex-column justify-center font-sans">
+//         <Link href={"/login"}>
+//           <h1 className="pt-10 text-gray-900">Welcome to BORROW!</h1>
+//         </Link>
+//       </section>
+//     </div>
+//   );
+// };
+
+// export default Index;
 import { NextPage } from "next";
 import Link from "next/link";
+
 interface Props {}
 
 const Index: NextPage<Props> = ({}) => {
   return (
-    <div className="bg-[#F5F5F5] max-w-sm h-screen">
-      <Header></Header>
-
-      <section className="flex flex-column justify-center font-sans">
+    <div className="">
+      <div className="">
+        <h1 className=" text-amber-600 w-72 h-20 text-7xl place-self-center font-minFont">
+          Borrow
+        </h1>
+        <p>Låna och låna ut inom din bostadsförening!</p>
         <Link href={"/login"}>
-          <h1 className="pt-10 text-gray-900">Welcome to BORRO!</h1>
+          <button>Kom igång!</button>
         </Link>
-      </section>
+      </div>
     </div>
   );
 };
