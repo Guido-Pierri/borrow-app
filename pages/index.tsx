9a736886d192c010271bd8897b69b0a0464ebd54
Alex <Alex.Paulsson@stud.sti.se>
2023-04-12 12:16:43 +0200

    Tog bort W

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
-import Products from "@/p-components/products"
-import Header from "@/p-components/header"
-import { NextPage } from "next"
-import Link from "next/link"
+import Products from "@/p-components/products";
+import Header from "@/p-components/header";
+import { NextPage } from "next";
+import Link from "next/link";
 interface Props {}
 
 const Index: NextPage<Props> = ({}) => {
@@ -11,11 +11,11 @@ const Index: NextPage<Props> = ({}) => {
 
       <section className="flex flex-column justify-center font-sans">
         <Link href={"/login"}>
-          <h1 className="pt-10 text-gray-900">Welcome to BORROW!</h1>
+          <h1 className="pt-10 text-gray-900">Welcome to BORRO!</h1>
         </Link>
       </section>
     </div>
-  )
-}
+  );
+};
 
-export default Index
+export default Index;
