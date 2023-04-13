eb4f7ccdc8e96c1dae68af8103b042cffe703908
Guido Pierri <105157860+Guido-Pierri@users.noreply.github.com>
2023-04-13 15:00:14 +0200

    done

--- pages/ads/index.tsx
+++ pages/ads/index.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link"
 import Header from "@/p-components/header"
 import clientPromise from "@/lib/mongodb"
+import { GoSearch } from "react-icons/go"
 interface Ad {
   id: string
   name: string
@@ -15,16 +16,74 @@ export default function Ads({ ads }: Props) {
   return (
     <div className="bg-[#F5F5F5] text-center max-w-sm h-screen ">
       <Header></Header>
-      <p className="font-sans text-4xl text-[#0f0e0e] ">Annonser</p>
+      {/* <form>
+        <div className="">
+          <label className="">
+            <input
+              className="bg-[#E6E6E6] font-sans
+              placeholder-black px-1 rounded-sm "
+              type="text"
+              placeholder="Sök..."
+            />
+            <div className="text-right">
+              <GoSearch />
+            </div>
+          </label>
+        </div>
+      </form> */}
+      <form>
+        <label className="relative">
+          <input
+            className="bg-[#E6E6E6] font-sans placeholder-black px-6 py-2 rounded-sm w-80"
+            type="text"
+            placeholder="Sök..."
+          />
+          <div className="absolute inset-y-0 right-0 flex items-center px-2">
+            <GoSearch />
+          </div>
+        </label>
+      </form>
+
+      <style jsx>{`
+        input[type="text"] {
+          background-repeat: no-repeat;
+          background-size: 16px 16px;
+          background-position: 8px 50%;
+        }
+      `}</style>
+
+      <div className="flex justify-center mt-5">
+        <Link href={"/createAd"}>
+          <button className="flex justify-center p-2 text-gray-900 bg-[#9EBB9D] w-[263px] rounded-sm text-xl font-[500] font-sans">
+            <p className=""> Skapa annons</p>
+          </button>
+        </Link>
+      </div>
+      <section className="flex justify-around mt-5 ">
+        <button className="mt-4 font-sans font-semibold border-4 px-4 py-1 border-[#46649D]">
+          LÅNA
+        </button>
+        <button className="mt-4 font-sans font-semibold border-2 px-4 py-1 border-black">
+          MINA ANNONSER
+        </button>
+        <button className="mt-4 font-sans font-semibold border-2 px-4 py-1 border-black">
+          TAVLAN
+        </button>
+      </section>
+      <section className="font-sans ">
+        <h2 className="text-left mt-4 px-4">Alla resultat:</h2>
+      </section>
       <div className=" font-sans">
-        <div className="mx-auto max-w-2xl px-4 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+        <div className="px-4">
           <div className="flex-column">
             {ads.map((ad) => (
               <a key={ad.id} className="group">
                 <div className="">
-                  <div className="mt-4  bg-[#9EBB9D] rounded-md">
+                  <div className="mt-4 rounded-sm border-[#46649D] border-2">
                     <p className="bold text-[#0f0e0e]"> Namn: {ad.name}</p>
-                    <p className="text-[#0f0e0e]">Beskrivning: {ad.description}</p>
+                    <p className="text-[#0f0e0e]">
+                      Beskrivning: {ad.description}
+                    </p>
                     <p className="text-[#0f0e0e]">Annonsör: {ad.username}</p>
                     <p className="text-[#0f0e0e]">Kontakt: {ad.useremail}</p>
                   </div>
@@ -33,13 +92,6 @@ export default function Ads({ ads }: Props) {
             ))}
           </div>
         </div>
-      </div>
-      <div className="flex justify-center mt-5">
-        <Link href={"/createAd"}>
-          <button className="flex justify-center p-2 text-gray-900 bg-[#9EBB9D] rounded-md text-xl font-[500] font-sans">
-            <p className=""> Skapa en annons</p>
-          </button>
-        </Link>
       </div>
     </div>
   )
