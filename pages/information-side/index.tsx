8096549b6fabf0842b4059a80cda8ea909fb92d1
Alex <Alex.Paulsson@stud.sti.se>
2023-04-19 11:26:21 +0200

    Fixed text positioning

--- pages/information-side/index.tsx
+++ pages/information-side/index.tsx
@@ -12,7 +12,7 @@ const Index: NextPage<Props> = ({}) => {
           <h1 className="font-bold text-xl pt-4">Så fungerar Borrow</h1>
         </div>
         <div>
-          <p className="py-4 px-1">
+          <p className="py-4 px-4">
             Borrow är en tjänst som ger möjligheten för dig som student att
             dryga ut kassan genom att låna och låna ut saker inom din
             studentförening. Utöver detta bidrar du också till en mer hållbar
@@ -20,20 +20,20 @@ const Index: NextPage<Props> = ({}) => {
           </p>
         </div>
         <div>
-          <h2 className="font-bold text-base pt-4 px-1">
+          <h2 className="font-bold text-base pt-4 px-4">
             För att använda Borrow behöver du:
           </h2>
           <div className="pt-1">
-            <p className="pt-3 px-3">
+            <p className="pt-3 px-6">
               1. Först registrera sig som medlem på hemsidan och ansluta sig
               till sin studentförening
             </p>
-            <p className="pt-3 px-3">
+            <p className="pt-3 px-6">
               2. Därefter kan man lägga upp saker som man vill låna ut eller
               söka efter saker som man vill låna. Det kan vara allt från böcker
               och kursmaterial till verktyg och sportutrustning.
             </p>
-            <p className="px-3 pt-3">
+            <p className="px-6 pt-3">
               3. När man har hittat en sak som man vill låna eller låna ut så
               kontaktar man personen genom Borrow och bestämmer tid och plats
               för utlåning/lån.
