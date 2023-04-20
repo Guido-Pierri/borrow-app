920ef79dded5920e076636ccd9db08ed738ca5e2
Guido Pierri <105157860+Guido-Pierri@users.noreply.github.com>
2023-04-20 09:40:34 +0200

    login success

--- pages/api/user/index.ts
+++ pages/api/user/index.ts
@@ -1,8 +1,7 @@
 import clientPromise from '@/lib/mongodb'
 import { User } from '@/types/user'
-import { request } from 'http'
-import { connect } from 'http2'
 import { NextApiRequest, NextApiResponse } from 'next'
+import { Elsie } from 'next/font/google'
 
 export default async function handler(
   req: NextApiRequest,
@@ -11,31 +10,49 @@ export default async function handler(
   try {
     const db = (await clientPromise).db
     switch (req.method) {
-      case 'GET': {
-        // Get all users from the database
-
+      case 'POST': {
+        console.log(req.body)
+        const { email, password } = req.body as User
         const client = await clientPromise
         const database = client.db('borrow')
         const collection = database.collection('users')
-        const users = await collection.find().toArray()
-        const convertedUsers: User[] = users.map((userDoc) => {
-          //what does this code do?
-          return {
-            name: userDoc.name as string,
-            password: userDoc.password as string,
-            email: userDoc.email as string,
-            age: userDoc.age as number,
-            userId: userDoc.userid as string,
-          }
-        })
-        if (convertedUsers.length === 0) {
-          console.log('PRETTY EMPTY DUDE')
-          res.status(200).json('EMPTY')
-        } else {
-          res.status(200).json(convertedUsers)
+
+        const crypto = require('crypto')
+
+        // Generate a random salt
+        const salt = 1010
+
+        // The password to be hashed
+
+        // Create a hash using SHA-256 with the salt
+        const hash = crypto
+          .createHash('sha256')
+          .update(salt + password)
+          .digest('hex')
+
+        console.log('Salt:', salt)
+        console.log('Hash:', hash)
+
+        try {
+          console.log(req.body)
+          // const name = req.body.name
+          // const email = req.body.email
+          const result = await collection.findOne({
+            password: hash,
+          })
+
+          // res.json(result)
+          if (result) {
+            res.status(200).json('User found')
+          } else
+            res.status(500).json({ message: 'Wrong password.', result, hash })
+        } catch (error) {
+          console.error(error)
+          res.status(500).json({ message: 'An error occurred.' })
         }
         break
       }
+
       default: {
         // Return a 405 Method Not Allowed error for all other HTTP methods
         res.setHeader('Allow', ['GET', 'POST'])
