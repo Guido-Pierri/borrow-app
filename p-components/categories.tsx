4fe7f5899155bb52297dfe97b994396c4af9dc0e
kajzuz <kajsauzelac@gmail.com>
2023-04-28 22:44:02 +0200

    yupp

--- p-components/categories.tsx
+++ p-components/categories.tsx
@@ -1,26 +1,26 @@
-import { NextPage } from 'next'
-import Image from 'next/image'
-import Link from 'next/link'
+import { NextPage } from "next"
+import Image from "next/image"
+import Link from "next/link"
 
 interface Props {} //add link and other functionalities when needed
 
 const Categories = ({}) => {
   return (
     <div className="bg-[#FFFFFF]">
-      <h1 className="font-sans text-black text-[14px] font-bold text-left pt-6 pl-8 ">
+      {/* <h1 className="font-sans text-black text-[14px] font-bold text-left pt-6 pl-8 ">
         Kategorier
-      </h1>
-      <div className="flex flex-row py-8 justify-between ">
+      </h1> */}
+      <div className="flex flex-row py-12 pb-8 justify-between ">
         <div>
           <button>
             {/* <Link href={"/"}> */}
             <div className="pl-5 pt-2  border-[#9EBB9D] border-2 w-[74px] h-[76px] rounded-full flex justify-between">
               <Image
-                src={'/vaccum2.png'}
-                alt={'#'}
-                width={'25'}
-                height={'54'}
-                style={{ alignSelf: 'center' }}
+                src={"/vaccum2.png"}
+                alt={"#"}
+                width={"25"}
+                height={"54"}
+                style={{ alignSelf: "center" }}
               ></Image>
             </div>
             {/* </Link> */}
@@ -35,12 +35,12 @@ const Categories = ({}) => {
             {/* <Link href={"/"}> */}
             <div className="pl-3 pt-5 border-2  border-[#9EBB9D] w-[74px] h-[76px] rounded-full ">
               <Image
-                src={'/pickaxe.svg'}
-                alt={'#'}
-                width={'47'}
-                height={'38'}
+                src={"/pickaxe.svg"}
+                alt={"#"}
+                width={"47"}
+                height={"38"}
                 style={{
-                  alignSelf: 'center',
+                  alignSelf: "center",
                 }}
               ></Image>
             </div>
@@ -55,11 +55,11 @@ const Categories = ({}) => {
             {/* <Link href={"/"}> */}
             <div className=" pl-2 pt-6 border-2 border-[#9EBB9D] w-[74px] h-[76px] rounded-full ">
               <Image
-                src={'/bike.svg'}
-                alt={'#'}
-                width={'54'}
-                height={'32'}
-                style={{ alignSelf: 'center' }}
+                src={"/bike.svg"}
+                alt={"#"}
+                width={"54"}
+                height={"32"}
+                style={{ alignSelf: "center" }}
               ></Image>
             </div>
             {/* </Link> */}
@@ -74,11 +74,11 @@ const Categories = ({}) => {
             {/* <Link href={"/"}> */}
             <div className=" pl-2 pt-4 border-2 border-[#9EBB9D] w-[74px] h-[76px] rounded-full ">
               <Image
-                src={'/tv.svg'}
-                alt={'#'}
-                width={'47'}
-                height={'37'}
-                style={{ alignSelf: 'center' }}
+                src={"/tv.svg"}
+                alt={"#"}
+                width={"47"}
+                height={"37"}
+                style={{ alignSelf: "center" }}
               ></Image>
             </div>
             {/* </Link> */}
@@ -92,11 +92,11 @@ const Categories = ({}) => {
             {/* <Link href={"/"}> */}
             <div className="pl-3 border-2 border-[#9EBB9D] w-[74px] h-[76px] rounded-full flex justify-between">
               <Image
-                src={'/grill2.png'}
-                alt={'#'}
-                width={'37'}
-                height={'74'}
-                style={{ alignSelf: 'center' }}
+                src={"/grill2.png"}
+                alt={"#"}
+                width={"37"}
+                height={"74"}
+                style={{ alignSelf: "center" }}
               ></Image>
             </div>
             {/* </Link> */}
