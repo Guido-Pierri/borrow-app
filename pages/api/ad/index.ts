672370ade1c6dcc7469d80ca10c5458ecdf3de09
Guido Pierri <105157860+Guido-Pierri@users.noreply.github.com>
2023-05-01 17:40:59 +0200

    not working yet

--- pages/api/ad/index.ts
+++ pages/api/ad/index.ts
@@ -1,6 +1,6 @@
-import clientPromise from "@/lib/mongodb"
-import { Collection } from "mongodb"
-import { NextApiRequest, NextApiResponse } from "next"
+import clientPromise from '@/lib/mongodb'
+import { Collection } from 'mongodb'
+import { NextApiRequest, NextApiResponse } from 'next'
 
 export default async function handler(
   req: NextApiRequest,
@@ -9,40 +9,38 @@ export default async function handler(
   console.log(req.body)
 
   try {
-    if (req.method === "PATCH") {
-      const { image, id, title, description, fullName, email } = req.body
+    if (req.method === 'PATCH') {
+      const { image, id, title, description, fullName, email, publisher } =
+        req.body
       const client = await clientPromise
-      const database = client.db("borrow")
-      const collection = database.collection("ads")
+      const database = client.db('borrow')
+      const collection = database.collection('ads')
       console.log(req.body)
       console.log(id)
       const result = await collection.updateOne(
         { id: id },
         {
           $set: {
-            ads: [
-              {
-                image: image,
-                title: title,
-                description: description,
-                fullName: fullName,
-                email: email,
-              },
-            ],
+            image: image,
+            title: title,
+            description: description,
+            fullName: fullName,
+            email: email,
+            publisher: publisher,
           },
         }
       )
 
       // res.json(result)
-      res.status(201).json({ message: "Update was successfull.", result })
+      res.status(201).json({ message: 'Update was successfull.', result })
     }
-    if (req.method === "POST") {
+    if (req.method === 'POST') {
       console.log(req.body)
       const { image, id, title, description, fullName, email, publisher } =
         req.body
       const client = await clientPromise
-      const database = client.db("borrow")
-      const collection = database.collection("ads")
+      const database = client.db('borrow')
+      const collection = database.collection('ads')
       // const name = req.body.name
       // const email = req.body.email
       const result = await collection.insertOne({
@@ -57,10 +55,10 @@ export default async function handler(
 
       // res.json(result)
 
-      res.status(201).json({ message: "Ad created successfully." })
+      res.status(201).json({ message: 'Ad created successfully.', result })
     }
   } catch (error) {
     console.error(error)
-    res.status(500).json({ message: "An error occurred." })
+    res.status(500).json({ message: 'An error occurred.' })
   }
 }
