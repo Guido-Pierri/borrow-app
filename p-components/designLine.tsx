45682eef2bf4f34d0c58b9604e0173b77184fb17
kajzuz <kajsauzelac@gmail.com>
2023-05-04 12:00:22 +0200

    Updated view

---
+++ p-components/designLine.tsx
@@ -0,0 +1,16 @@
+import { NextPage } from "next"
+import Image from "next/image"
+
+interface Props {}
+
+const DesignLine = ({ style }: any) => {
+  return (
+    <div>
+      <div className="flex justify-center mt-2 h-4 w-96 pr-12">
+        <Image src={"/Line.svg"} alt={"#"} width={"347"} height={"280"}></Image>
+      </div>
+    </div>
+  )
+}
+
+export default DesignLine
