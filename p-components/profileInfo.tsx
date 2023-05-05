cc411e41596b1f41637b1cdf0d4a7c56e1712dfd
Guido Pierri <105157860+Guido-Pierri@users.noreply.github.com>
2023-05-05 15:58:06 +0200

    added submit button to my profile info

--- p-components/profileInfo.tsx
+++ p-components/profileInfo.tsx
@@ -13,7 +13,7 @@ export default function ProfileInfo({
       {/* <HeaderWithCloseIcon /> */}
 
       <form
-        className="font-sans bg-[#FFFFFF] ml-[6.7%] mr-[6.9%] flex flex-col h-[75.83%]"
+        className="font-sans bg-[#FFFFFF] ml-[6.7%] mr-[6.9%] flex flex-col h-[640px]"
         //  onSubmit={handleSubmit}
       >
         <div className="mt-[6.9%] mr-[5.8%] flex flex-row-reverse justify-between ">
@@ -94,6 +94,37 @@ export default function ProfileInfo({
             style={{ color: '#000000' }}
           />
         </label>
+        <button className="mt-[1.56%]  rounded-[2px] bg-[#9EBB9D] placeholder-[#000000] w-full h-[55px] flex items-center justify-center">
+          <div className=" ">
+            <svg
+              width="27"
+              height="27"
+              viewBox="0 0 27 27"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              xmlnsXlink="http://www.w3.org/1999/xlink"
+            >
+              <rect width="27" height="27" fill="url(#pattern0)" />
+              <defs>
+                <pattern
+                  id="pattern0"
+                  patternContentUnits="objectBoundingBox"
+                  width="1"
+                  height="1"
+                >
+                  <use xlinkHref="#image0_2042_4955" transform="scale(0.02)" />
+                </pattern>
+                <image
+                  id="image0_2042_4955"
+                  width="50"
+                  height="50"
+                  xlinkHref="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADIAAAAyCAYAAAAeP4ixAAAACXBIWXMAAAsTAAALEwEAmpwYAAABXUlEQVR4nO2WsUoEMRCGP7a95tAHsLBRH0TFRnyDrXwBQVTUQtQr9xFELRSsPMXGQgQbLaw9Oxtf4kQlEHEIe3Amkp2F+WAgu2Qm8+fPkgXDMAzjl0ngFDgGJmgxu8CXjx1aTCWEuHFrqUyIMswRbZgj2jBHtGGOaMMc0YY5og1zRButc2QZeANugO4YQtyca+AdWEER96LhJyGmTkjXz/l5/4Ai1kRjUkwoJBThYh1l9IIGn4ET8XwEPAZz1H43VdDo54ixahGjnKmLrCKmgXNgDygSnUkRUQDb/phOEcGFWHwpIr/6JycWRf5ZRD53okDZ4IVYihqup0aEOBaA+YT8UouQVEoT4rkSO7FBc2yJPi5T74MXoEN+OsBA9HEQU2QO+BBFBt6Z1UyxCbyK9YfATOyOHI5xS+eKfRIovJ3Smdwx9CL++ndRy6x3pw/cZoq+38To42QYhoFavgF/GiSkHOBdGAAAAABJRU5ErkJggg=="
+                />
+              </defs>
+            </svg>
+          </div>
+          <p>Spara ändring</p>
+        </button>
         {/*
          <label>
           <input
