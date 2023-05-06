3c770ba6e2325da685388938c13b599d140df884
kajzuz <kajsauzelac@gmail.com>
2023-05-06 19:20:18 +0200

    Some CSS

--- p-components/logIn.tsx
+++ p-components/logIn.tsx
@@ -1,12 +1,12 @@
-import Link from 'next/link'
-import { useState } from 'react'
+import Link from "next/link"
+import { useState } from "react"
 
-import { LogIn } from '@/types/logIns'
+import { LogIn } from "@/types/logIns"
 
 export default function Login() {
   const [formData, setFormData] = useState<LogIn>({
-    email: '',
-    password: '',
+    email: "",
+    password: "",
   })
 
   console.log(formData)
@@ -18,21 +18,21 @@ export default function Login() {
       password: formData.password,
     }
 
-    const response = await fetch('/api/user', {
-      method: 'POST',
+    const response = await fetch("/api/user", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
       body: JSON.stringify(apiData),
     })
 
     const data = await response.json()
 
-    console.log('data', response)
+    console.log("data", response)
     if (response.ok) {
       window.location.href = `/ads/${data}`
     } else {
-      alert('Inloggning mysslyckades')
+      alert("Inloggning mysslyckades")
     }
   }
 
@@ -59,7 +59,7 @@ export default function Login() {
         <form onSubmit={handleSubmit}>
           <label>
             <input
-              className="rounded py-4 px-7 mt-8 border w-[265px] border-[#9EBB9D] placeholder-[#000000] bg-[#fff]"
+              className="rounded py-4 px-7 mt-8 border w-[265px] outline-[#9EBB9D] border-[#9EBB9D] placeholder-[#000000] bg-[#fff]"
               placeholder="E-post..."
               type="email"
               name="email"
@@ -72,7 +72,7 @@ export default function Login() {
           </label>
           <label>
             <input
-              className="rounded py-4 px-7 mt-8 border w-[265px] border-[#9EBB9D] placeholder-[#000000] bg-[#fff]"
+              className="rounded py-4 px-7 mt-8 border w-[265px] outline-[#9EBB9D] border-[#9EBB9D] placeholder-[#000000] bg-[#fff]"
               placeholder="Lösenord..."
               type="password"
               name="password"
@@ -95,8 +95,8 @@ export default function Login() {
               Logga in
             </button>
             <p className="mt-20 text-sm text-black">
-              Har du inget konto än?{' '}
-              <Link href={'/register-site'}>
+              Har du inget konto än?{" "}
+              <Link href={"/register-site"}>
                 <span className="text-[#0074B6] font-medium underline text-md">
                   Registrera dig här
                 </span>
--- pages/ads/myAds/[userId].tsx
+++ pages/ads/myAds/[userId].tsx
@@ -1,16 +1,16 @@
-import Link from 'next/link'
-import Header from '@/p-components/header'
-import { GoSearch } from 'react-icons/go'
-import { Ad } from '@/types/ads'
-import { CldImage } from 'next-cloudinary'
-import Image from 'next/image'
-import { Suspense } from 'react'
-import Categories from '@/p-components/categories'
-import clientPromise from '@/lib/mongodb'
-import { useRouter } from 'next/router'
-import { ObjectId } from 'mongodb'
-import DesignLine from '@/p-components/designLine'
-import ButtonCreateAd from '@/p-components/buttonCreateAd'
+import Link from "next/link"
+import Header from "@/p-components/header"
+import { GoSearch } from "react-icons/go"
+import { Ad } from "@/types/ads"
+import { CldImage } from "next-cloudinary"
+import Image from "next/image"
+import { Suspense } from "react"
+import Categories from "@/p-components/categories"
+import clientPromise from "@/lib/mongodb"
+import { useRouter } from "next/router"
+import { ObjectId } from "mongodb"
+import DesignLine from "@/p-components/designLine"
+import ButtonCreateAd from "@/p-components/buttonCreateAd"
 
 interface AdId {
   id: string
@@ -36,20 +36,20 @@ const Ads = ({ ads }: Props) => {
     window.location.href = `/ads/${id}`
   }
   const handleClick = async () => {
-    console.log('insede handleClick')
+    console.log("insede handleClick")
     console.log(`${userId}`)
 
     const response = await fetch(`/api/user/${userId}`, {
-      method: 'POST',
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
       body: JSON.stringify(`${userId}`),
     })
 
     const dataResponse = await response.json()
 
-    console.log('dataResponse', dataResponse)
+    console.log("dataResponse", dataResponse)
     if (dataResponse) {
     }
   }
@@ -63,11 +63,11 @@ const Ads = ({ ads }: Props) => {
       <form>
         <label className="relative">
           <input
-            className="bg-[#E6E6E6] font-sans placeholder-black px-6 py-2 rounded-sm w-80"
+            className="bg-[#E6E6E6] font-sans outline-[#9EBB9D] placeholder-black px-6 py-2 rounded-sm w-80"
             type="text"
             placeholder="Sök..."
           />
-          <div className="absolute inset-y-0 right-0 flex items-center px-2 text-black">
+          <div className="absolute inset-y-0  right-0 flex items-center px-2 text-black">
             <GoSearch />
           </div>
         </label>
@@ -76,7 +76,7 @@ const Ads = ({ ads }: Props) => {
       {/* <Categories></Categories> */}
 
       <style jsx>{`
-        input[type='text'] {
+        input[type="text"] {
           background-repeat: no-repeat;
           background-size: 16px 16px;
           background-position: 8px 50%;
@@ -131,8 +131,8 @@ const Ads = ({ ads }: Props) => {
                   className="w-full rounded-md"
                   alt={ad.description}
                   src={ad.image}
-                  width={'100'}
-                  height={'100'}
+                  width={"100"}
+                  height={"100"}
                 />
                 <DesignLine></DesignLine>
                 {/* 
@@ -190,10 +190,10 @@ export async function getServerSideProps(context: any) {
   try {
     const { userId } = context.query
     const client = await clientPromise
-    const db = client.db('borrow')
+    const db = client.db("borrow")
 
     const ads = await db
-      .collection('ads')
+      .collection("ads")
       .find({ publisher: userId })
       .sort({ _id: -1 })
       .limit(1000)
