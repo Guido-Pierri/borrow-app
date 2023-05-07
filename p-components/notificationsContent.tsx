181a7308272d935312cfbb1447f1e01648bd0552
Guido Pierri <105157860+Guido-Pierri@users.noreply.github.com>
2023-05-07 21:26:18 +0200

    margins notifications

--- p-components/notificationsContent.tsx
+++ p-components/notificationsContent.tsx
@@ -17,7 +17,7 @@ const NotificationsContent: FC<NotificationsContentProps> = ({ onClose }) => {
 
   return (
     <>
-      <div className="font-sans bg-[#FFFFFF] flex flex-col h-[640px] w-[full] rounded-lg mx-[11.5%]">
+      <div className="font-sans bg-[#FFFFFF] flex flex-col h-[full] w-[full] rounded-lg ml-[5.7%] mr-[5.7%] my-[12%]">
         <div className="mt-[25px] ml-[10%] mr-[5.8%] flex flex-row justify-between ">
           <p className="text-xl font-[700] text-black flex justify-start">
             Mina uppgifter
