a978cca7dbf6b649f5a18b20364b9f18668c4c79
Guido Pierri <105157860+Guido-Pierri@users.noreply.github.com>
2023-05-10 10:51:34 +0200

    not ready

--- p-components/deleteAccountContent.tsx
+++ p-components/deleteAccountContent.tsx
@@ -1,11 +1,54 @@
+import clientPromise from '@/lib/mongodb'
+import { User } from '@/types/user'
+import { UserId } from '@/types/userId'
+import { Db } from 'mongodb'
+import { ObjectId } from '@/types/objectId'
 import { NextPage, GetStaticPaths } from 'next'
 import Image from 'next/image'
-import { FC } from 'react'
+import { FC, FormEvent } from 'react'
 
 interface DeleteAccountContentProps {
   onClose: () => void
+  userId: string
 }
-const DeleteAccountContent: FC<DeleteAccountContentProps> = ({ onClose }) => {
+const DeleteAccountContent: FC<DeleteAccountContentProps> = ({
+  onClose,
+  userId,
+}: DeleteAccountContentProps) => {
+  function onClickHandler() {
+    deleteAccount(userId)
+    //  onClose()
+  }
+  const deleteAccount = async (userId: string) => {
+    console.log('inside deleteAccount function')
+
+    console.log('_id:', userId)
+
+    console.log('apiData:', '645b402a1465885c42c6ba16')
+    const apiData = { _id: '645b402a1465885c42c6ba16' }
+    try {
+      console.log('apiData:', apiData)
+
+      const response = await fetch('/api/user', {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(apiData),
+      })
+      if (!response.ok) {
+        throw new Error(`Failed to delete account with userId ${userId}`)
+      }
+      const data = await response.json()
+      console.log(data)
+      if (data) {
+      }
+      alert('Konto avslutat')
+    } catch (error) {
+      console.error(error)
+      // handle error
+    }
+  }
   return (
     <>
       <div className="font-sans bg-[#FFFFFF] flex flex-col h-[24%] w-[full] rounded-lg ml-[5.7%] mr-[5.7%] ">
@@ -20,7 +63,10 @@ const DeleteAccountContent: FC<DeleteAccountContentProps> = ({ onClose }) => {
             <button className="border rounded-md border-[#9EBB9D] w-[34%] h-[26px] mt-[4%] mb-[15%] text-center">
               <p className="text-[10px] font-medium">Avbryt</p>
             </button>
-            <button className="bg-[#9EBB9D] rounded-md w-[34%] h-[26px] text-center mt-[4%] mb-[15%]">
+            <button
+              className="bg-[#9EBB9D] rounded-md w-[34%] h-[26px] text-center mt-[4%] mb-[15%]"
+              onClick={onClickHandler}
+            >
               <p className="text-[10px] font-medium">Avsluta</p>
             </button>
           </div>
--- pages/api/user/index.ts
+++ pages/api/user/index.ts
@@ -2,20 +2,23 @@ import hashning from '@/lib/functions/hashning'
 import clientPromise from '@/lib/mongodb'
 import { LogIn } from '@/types/logIns'
 import { User } from '@/types/user'
+import { ObjectId } from 'mongodb'
 import { NextApiRequest, NextApiResponse } from 'next'
+import { Result } from 'postcss'
+import { AiOutlineDeploymentUnit } from 'react-icons/ai'
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  const client = await clientPromise
+  const database = client.db('borrow')
+  const collection = database.collection('users')
   try {
     switch (req.method) {
       case 'POST': {
         console.log(req.body)
         const { email, password } = req.body as LogIn
-        const client = await clientPromise
-        const database = client.db('borrow')
-        const collection = database.collection('users')
         const user = await collection.findOne(
           { email: email },
           { projection: { password: 1 } }
@@ -37,11 +40,14 @@ export default async function handler(
 
         break
       }
+      case 'DELETE': {
+        const { ObjectId } = require('mongodb')
+        const { _id } = req.body
 
-      default: {
-        // Return a 405 Method Not Allowed error for all other HTTP methods
-        res.setHeader('Allow', ['GET', 'POST'])
-        res.status(405).end(`Method ${req.method} Not Allowed`)
+        const objectId = new ObjectId(_id)
+        const result = await collection.deleteOne({ _id: objectId })
+        res.json({ deletedCount: result.deletedCount })
+
         break
       }
     }
