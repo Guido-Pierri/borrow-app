eef9712e58a85b362269857c8f6c0213b3c13ddf
Guido Pierri <105157860+Guido-Pierri@users.noreply.github.com>
2023-05-12 15:09:12 +0200

    compile error fix

--- pages/board/[userId].tsx
+++ pages/board/[userId].tsx
@@ -1,20 +1,20 @@
-import ButtonCreateAd from "@/p-components/buttonCreateAd";
-import Categories from "@/p-components/categories";
-import Header from "@/p-components/header";
-import { NextPage } from "next";
-import Link from "next/link";
-import router, { useRouter } from "next/router";
-import { GoSearch } from "react-icons/go";
-import { MongoClient, Db } from "mongodb";
-import clientPromise from "@/lib/mongodb";
-import { BoardAd } from "@/types/boardAd";
-import { useState } from "react";
-import SearchBar from "@/p-components/searchBar";
-import Image from "next/image";
-import DesignLine from "@/p-components/designLine";
+import ButtonCreateAd from '@/p-components/buttonCreateAd'
+import Categories from '@/p-components/categories'
+import Header from '@/p-components/header'
+import { NextPage } from 'next'
+import Link from 'next/link'
+import router, { useRouter } from 'next/router'
+import { GoSearch } from 'react-icons/go'
+import { MongoClient, Db } from 'mongodb'
+import clientPromise from '@/lib/mongodb'
+import { BoardAd } from '@/types/boardAd'
+import { useState } from 'react'
+import SearchBar from '@/p-components/searchBar'
+import Image from 'next/image'
+import DesignLine from '@/p-components/designLine'
 
 interface Props {
-  boardAds: BoardAd[];
+  boardAds: BoardAd[]
 }
 
 // function navigateToAd(id: string) {
@@ -22,10 +22,10 @@ interface Props {
 // }
 
 const Board = ({ boardAds }: Props) => {
-  const [query, setQuery] = useState("");
-  const [selectedCategory, setSelectedCategory] = useState("");
-  const router = useRouter();
-  const { userId } = router.query;
+  const [query, setQuery] = useState('')
+  const [selectedCategory, setSelectedCategory] = useState('')
+  const router = useRouter()
+  const { userId } = router.query
   const filteredBoardAds = boardAds.filter((boardAd) =>
     boardAd.title.includes(
       query
@@ -36,7 +36,7 @@ const Board = ({ boardAds }: Props) => {
 
         .concat(query.charAt(1).toLocaleLowerCase())
     )
-  );
+  )
 
   // .filter(
   //   (ad) => !selectedCategory
@@ -44,7 +44,7 @@ const Board = ({ boardAds }: Props) => {
   //   //  ad.category === selectedCategory
   // );
 
-  console.log("selectedCategory:", selectedCategory);
+  console.log('selectedCategory:', selectedCategory)
 
   return (
     <>
@@ -91,7 +91,7 @@ const Board = ({ boardAds }: Props) => {
             <p className="font-normal text-base">Alla inlägg</p>
           </button>
           <div>
-            <Image src={"/Vector 88.svg"} width={1} height={"12"} alt={""} />
+            <Image src={'/Vector 88.svg'} width={1} height={'12'} alt={''} />
           </div>
           <Link href={`/board/myAdsBoard/${userId}`}>
             <button className="ml-[2%]">
@@ -106,10 +106,10 @@ const Board = ({ boardAds }: Props) => {
                 <div>
                   <Image
                     className="ml-[5.6%]"
-                    src={"/Line.svg"}
-                    alt={"#"}
-                    width={"347"}
-                    height={"280"}
+                    src={'/Line.svg'}
+                    alt={'#'}
+                    width={'347'}
+                    height={'280'}
                   ></Image>
                 </div>
                 <div className="flex items-center">
@@ -117,10 +117,9 @@ const Board = ({ boardAds }: Props) => {
                     // onClick={() => navigateToAd(ad.id)}
                     className="ml-[7%] mr-[3.5%] mt-[4%]"
                     alt={boardAd.description}
-                    src={"/Profil.svg"}
-                    width={"75"}
-                    height={"98"}
-                    property={userId}
+                    src={'/Profil.svg'}
+                    width={'75'}
+                    height={'98'}
                   />
                   <div>
                     <p className="text-sm font-semibold mr-[7%]">
@@ -142,27 +141,27 @@ const Board = ({ boardAds }: Props) => {
         `}</style>
       </div>
     </>
-  );
-};
+  )
+}
 export async function getServerSideProps() {
   try {
-    const client = await clientPromise;
-    const db = client.db("borrow");
+    const client = await clientPromise
+    const db = client.db('borrow')
 
     const boardAds = await db
-      .collection("board")
+      .collection('board')
       .find({})
       .sort({ _id: -1 })
-      .toArray();
+      .toArray()
 
-    console.log(boardAds);
+    console.log(boardAds)
 
     return {
       props: { boardAds: JSON.parse(JSON.stringify(boardAds)) },
-    };
+    }
   } catch (e) {
-    console.error(e);
+    console.error(e)
   }
 }
 
-export default Board;
+export default Board
