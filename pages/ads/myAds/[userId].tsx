777c5d1d3c59bc34c19add8cd916504e78c44215
Guido Pierri <105157860+Guido-Pierri@users.noreply.github.com>
2023-05-12 13:59:06 +0200

    pixelpushing my ads and ads images

--- pages/ads/myAds/[userId].tsx
+++ pages/ads/myAds/[userId].tsx
@@ -1,18 +1,18 @@
-import Link from "next/link"
-import Header from "@/p-components/header"
-import { GoSearch } from "react-icons/go"
-import { Ad } from "@/types/ads"
-import { CldImage } from "next-cloudinary"
-import Image from "next/image"
-import { Suspense } from "react"
-import Categories from "@/p-components/categories"
-import clientPromise from "@/lib/mongodb"
-import { useRouter } from "next/router"
-import { ObjectId } from "mongodb"
-import DesignLine from "@/p-components/designLine"
-import ButtonCreateAd from "@/p-components/buttonCreateAd"
-import hashning from "@/lib/functions/hashning"
-import IconAdsEmpty from "@/p-components/iconAdsEmpty"
+import Link from 'next/link'
+import Header from '@/p-components/header'
+import { GoSearch } from 'react-icons/go'
+import { Ad } from '@/types/ads'
+import { CldImage } from 'next-cloudinary'
+import Image from 'next/image'
+import { Suspense } from 'react'
+import Categories from '@/p-components/categories'
+import clientPromise from '@/lib/mongodb'
+import { useRouter } from 'next/router'
+import { ObjectId } from 'mongodb'
+import DesignLine from '@/p-components/designLine'
+import ButtonCreateAd from '@/p-components/buttonCreateAd'
+import hashning from '@/lib/functions/hashning'
+import IconAdsEmpty from '@/p-components/iconAdsEmpty'
 
 interface AdId {
   id: string
@@ -38,20 +38,20 @@ const Ads = ({ ads }: Props) => {
     window.location.href = `/ads/${id}`
   }
   const handleClick = async () => {
-    console.log("insede handleClick")
+    console.log('insede handleClick')
     console.log(`${userId}`)
 
     const response = await fetch(`/api/user/${userId}`, {
-      method: "POST",
+      method: 'POST',
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
       body: JSON.stringify(`${userId}`),
     })
 
     const dataResponse = await response.json()
 
-    console.log("dataResponse", dataResponse)
+    console.log('dataResponse', dataResponse)
     if (dataResponse) {
     }
   }
@@ -79,7 +79,7 @@ const Ads = ({ ads }: Props) => {
       {/* <Categories></Categories> */}
 
       <style jsx>{`
-        input[type="text"] {
+        input[type='text'] {
           background-repeat: no-repeat;
           background-size: 16px 16px;
           background-position: 8px 50%;
@@ -103,7 +103,7 @@ const Ads = ({ ads }: Props) => {
         >
           Mina annonser
         </button>
-        <Link href={"/board/" + `${userId}`}>
+        <Link href={'/board/' + `${userId}`}>
           <button className="rounded-t-md -md mt-4 font-sans font-semibold   px-4 py-1  text-black">
             Tavlan
           </button>
@@ -122,7 +122,7 @@ const Ads = ({ ads }: Props) => {
         </button> */}
       </div>
 
-      {ads.length === 0 ? <IconAdsEmpty></IconAdsEmpty> : ""}
+      {ads.length === 0 ? <IconAdsEmpty></IconAdsEmpty> : ''}
 
       <section>
         <div className=" font-sans text-left  mt-8">
@@ -135,11 +135,11 @@ const Ads = ({ ads }: Props) => {
               <div className="pl-6">
                 <Image
                   // onClick={() => navigateToAd(ad.id)}
-                  className="w-full rounded-md"
+                  className="w-full rounded-sm"
                   alt={ad.description}
                   src={ad.image}
-                  width={"100"}
-                  height={"100"}
+                  width={'100'}
+                  height={'100'}
                 />
                 <DesignLine></DesignLine>
                 {/* 
@@ -197,10 +197,10 @@ export async function getServerSideProps(context: any) {
   try {
     const { userId } = context.query
     const client = await clientPromise
-    const db = client.db("borrow")
+    const db = client.db('borrow')
 
     const ads = await db
-      .collection("ads")
+      .collection('ads')
       .find({ publisher: userId })
       .sort({ _id: -1 })
       .limit(1000)
