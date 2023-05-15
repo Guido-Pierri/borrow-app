cc8af30aca664cae5c80eab2a56611691ae209cc
Guido Pierri <105157860+Guido-Pierri@users.noreply.github.com>
2023-05-15 13:07:05 +0200

    login header updated

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,12 +1,12 @@
 import { NextPage } from 'next'
 import LogIn from '@/p-components/logIn'
-import Header from '@/p-components/header'
+import HeaderProfile from '@/p-components/HeaderProfile'
 interface Props {}
 
 const Index: NextPage<Props> = ({}) => {
   return (
     <div className="max-w-sm">
-      <Header></Header>
+      <HeaderProfile userId={''} anotherUserId={0} />
       <LogIn></LogIn>
     </div>
   )
