24108833666da120e75729cc32ba8b7948aff8ec
Guido Pierri <105157860+Guido-Pierri@users.noreply.github.com>
2023-05-15 11:45:21 +0200

    imformation-site layout update

--- pages/information-side/[userId].tsx
+++ pages/information-side/[userId].tsx
@@ -16,34 +16,37 @@ const Index: NextPage<Props> = () => {
     <div className="h-screen bg-[#FFFFFF] max-w-sm">
       <HeaderWithCloseIcon userId={userId} />
       <div className="font-sans">
-        <div className="px-4 ">
+        <div className="ml-[11.8%] ">
           <h1 className="font-bold text-xl pt-4 text-black">
             Så fungerar Borrow
           </h1>
         </div>
-        <div>
-          <p className="py-4 px-4 text-black">
+        <div className="mx-[11.8%] mt-[3.9%] text-black">
+          <p>
             Borrow är en tjänst som ger möjligheten för dig som student att
             dryga ut kassan genom att låna och låna ut saker inom din
-            studentförening. Utöver detta bidrar du också till en mer hållbar
-            framtid och ökad gemenskap i ditt grannskap.
+            studentförening.
+          </p>
+          <p className="mt-[3.9%]">
+            Utöver detta bidrar du också till en mer hållbar framtid och ökad
+            gemenskap i ditt grannskap.
           </p>
         </div>
-        <div>
-          <h2 className="font-bold text-base pt-4 px-4 text-black">
+        <div className="mt-[8%]">
+          <h2 className="font-bold text-base mx-[11.8%]  text-black">
             För att använda Borrow behöver du:
           </h2>
-          <div className="pt-1">
-            <p className="pt-3 px-6 text-black">
+          <div className="mx-[11.8%] ">
+            <p className=" text-black mt-[8%]">
               1. Först registrera sig som medlem på hemsidan och ansluta sig
               till sin studentförening
             </p>
-            <p className="pt-3 px-6 text-black">
+            <p className=" text-black mt-[3.9%]">
               2. Därefter kan man lägga upp saker som man vill låna ut eller
               söka efter saker som man vill låna. Det kan vara allt från böcker
               och kursmaterial till verktyg och sportutrustning.
             </p>
-            <p className="px-6 pt-3 text-black">
+            <p className=" text-black mt-[3.9%]">
               3. När man har hittat en sak som man vill låna eller låna ut så
               kontaktar man personen genom Borrow och bestämmer tid och plats
               för utlåning/lån.
