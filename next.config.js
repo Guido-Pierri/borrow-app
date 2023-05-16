469fdf6122da8e901d444ab9714cf384c6d9b122
Guido Pierri <105157860+Guido-Pierri@users.noreply.github.com>
2023-05-16 13:39:10 +0200

    favicon

--- next.config.js
+++ next.config.js
@@ -6,4 +6,16 @@ module.exports = {
   images: {
     domains: ['res.cloudinary.com'],
   },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.(ico|png|svg)$/,
+      loader: 'file-loader',
+      options: {
+        publicPath: '/_next',
+        name: 'static/media/[name].[hash].[ext]',
+      },
+    })
+
+    return config
+  },
 }
