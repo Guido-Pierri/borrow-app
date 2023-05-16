dfae6c132da49468d5a9fbcc10903121aacf3ea6
Guido Pierri <105157860+Guido-Pierri@users.noreply.github.com>
2023-05-16 11:08:16 +0200

    api/boardAd/index

--- pages/api/boardAd/index.ts
+++ pages/api/boardAd/index.ts
@@ -1,5 +1,4 @@
 import clientPromise from '@/lib/mongodb'
-import { Collection } from 'mongodb'
 import { NextApiRequest, NextApiResponse } from 'next'
 
 export default async function handler(
@@ -7,30 +6,29 @@ export default async function handler(
   res: NextApiResponse
 ) {
   console.log(req.body)
-
   try {
-    if (req.method === 'PATCH') {
-      const { id, title, description, email, publisher } = req.body
-      const client = await clientPromise
-      const database = client.db('borrow')
-      const collection = database.collection('board')
-      console.log(req.body)
-      console.log(id)
-      const result = await collection.updateOne(
-        { id: id },
-        {
-          $set: {
-            title: title,
-            description: description,
-            email: email,
-            publisher: publisher,
-          },
-        }
-      )
+    //Kod som ska användas senare
+    // if (req.method === 'PATCH') {
+    //   const { id, title, description, email, publisher } = req.body
+    //   const client = await clientPromise
+    //   const database = client.db('borrow')
+    //   const collection = database.collection('board')
+    //   console.log(req.body)
+    //   console.log(id)
+    //   const result = await collection.updateOne(
+    //     { id: id },
+    //     {
+    //       $set: {
+    //         title: title,
+    //         description: description,
+    //         email: email,
+    //         publisher: publisher,
+    //       },
+    //     }
+    //   )
 
-      // res.json(result)
-      res.status(201).json({ message: 'Update was successfull.', result })
-    }
+    //   res.status(201).json({ message: 'Update was successfull.', result })
+    // }
     if (req.method === 'POST') {
       console.log(req.body)
       const {
@@ -44,8 +42,7 @@ export default async function handler(
       const client = await clientPromise
       const database = client.db('borrow')
       const collection = database.collection('board')
-      // const name = req.body.name
-      // const email = req.body.email
+
       const result = await collection.insertOne({
         id: id,
         title: title,
@@ -54,8 +51,6 @@ export default async function handler(
         publisher: publisher,
         publisherProfileImage: publisherProfileImage,
       })
-
-      // res.json(result)
 
       res.status(201).json({ message: 'Ad created successfully.', result })
     }
